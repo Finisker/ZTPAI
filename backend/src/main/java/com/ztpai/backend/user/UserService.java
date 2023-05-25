bb62b7e364062f7381778ed05b7aaf5c284a5291
Maciej Plachta <macpla08@gmail.com>
2023-05-25 17:44:28 +0200

    Learning how to database

---
+++ backend/src/main/java/com/ztpai/backend/user/UserService.java
@@ -0,0 +1,18 @@
+//package com.ztpai.backend.user;
+//
+//import org.springframework.stereotype.Service;
+//
+//import java.util.List;
+//
+//@Service
+//public class UserService {
+//    public List<MyUser> getUsers(){
+//        return List.of(
+//                new MyUser(
+//                        1L,
+//                        "Test",
+//                        "Test"
+//                )
+//        );
+//    }
+//}
