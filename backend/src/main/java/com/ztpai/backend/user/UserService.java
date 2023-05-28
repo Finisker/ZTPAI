747d8cf5d7e3b939bac99252e0abca7edc88bdd8
Maciej Plachta <macpla08@gmail.com>
2023-05-28 22:11:53 +0200

    added unique login validation

--- backend/src/main/java/com/ztpai/backend/user/UserService.java
+++ backend/src/main/java/com/ztpai/backend/user/UserService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -20,6 +21,12 @@ public class UserService {
     }
 
     public void addNewUser(MyUser user) {
+        Optional<MyUser> userOptional = userRepository.findUserByLogin(user.getLogin());
+
+        if (userOptional.isPresent()){
+            throw new IllegalStateException("login taken");
+        }
+
         userRepository.save(user);
     }
 }
