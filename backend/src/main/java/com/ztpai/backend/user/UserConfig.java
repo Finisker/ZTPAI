5941d12e626d79593cdbbacb2277c57972da7ee3
Maciej Plachta <macpla08@gmail.com>
2023-05-28 21:58:41 +0200

    Testing UserConfig to learn how it works

---
+++ backend/src/main/java/com/ztpai/backend/user/UserConfig.java
@@ -0,0 +1,20 @@
+package com.ztpai.backend.user;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.HashSet;
+
+@Configuration
+public class UserConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(UserRepository userRepository){
+        return args -> {
+            MyUser test = new MyUser("test2","test2",new HashSet<>());
+
+            userRepository.save(test);
+        };
+    }
+}
