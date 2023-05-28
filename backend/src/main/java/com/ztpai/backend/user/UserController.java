077342fca4ac0ea8769a1c86a2db919e671d25d4
Maciej Plachta <macpla08@gmail.com>
2023-05-28 21:49:26 +0200

    Added UserController with getUsers

--- backend/src/main/java/com/ztpai/backend/user/UserController.java
+++ backend/src/main/java/com/ztpai/backend/user/UserController.java
@@ -1,26 +1,26 @@
-//package com.ztpai.backend.user;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//import java.util.List;
-//
-//@RestController
-//@RequestMapping(path = "api/v1/user")
-//public class UserController {
-//
-//    private final UserService userService;
-//
-//    @Autowired
-//    public UserController(UserService userService) {
-//        this.userService = userService;
-//    }
-//
-//    @GetMapping
-//    public List<MyUser> getUsers(){
-//        return userService.getUsers();
-//    }
-//
-//}
+package com.ztpai.backend.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/v1/user")
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<MyUser> getUsers() {
+        return userService.getUsers();
+    }
+
+}
