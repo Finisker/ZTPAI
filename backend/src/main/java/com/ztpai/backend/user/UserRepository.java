670f5ed2a83cea49300143acf9806a1ff946bb6f
Maciej Plachta <macpla08@gmail.com>
2023-05-28 22:11:07 +0200

    made finding users by login possible

--- backend/src/main/java/com/ztpai/backend/user/UserRepository.java
+++ backend/src/main/java/com/ztpai/backend/user/UserRepository.java
@@ -3,6 +3,10 @@ package com.ztpai.backend.user;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<MyUser,Long> {
+
+    Optional<MyUser> findUserByLogin(String login);
 }
