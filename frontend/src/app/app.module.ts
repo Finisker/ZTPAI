f320d324a9163d90cc8e3b6319b61ee9efcdae2d
maciejplachta <maciej.plachta@stazysta.comarch.pl>
2023-08-03 16:22:45 +0200

    Did this and that

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CharacterMiniatureComponent } from './character-miniature/character-miniature.component';
 import { FunctionalCharacterMiniatureComponent } from './functional-character-miniature/functional-character-miniature.component';
 import { SearchBarComponent } from './search-bar/search-bar.component';
 import { FilterMenuComponent } from './filter-menu/filter-menu.component';
@@ -13,7 +12,6 @@ import { MainMenuComponent } from './main-menu/main-menu.component';
 @NgModule({
   declarations: [
     AppComponent,
-    CharacterMiniatureComponent,
     FunctionalCharacterMiniatureComponent,
     SearchBarComponent,
     FilterMenuComponent,
