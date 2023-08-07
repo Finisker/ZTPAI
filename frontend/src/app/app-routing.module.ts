786d5b0ea078b4877cd1be7c5f954b88f40f04a3
maciejplachta <maciej.plachta@stazysta.comarch.pl>
2023-08-07 12:45:02 +0200

    Routing tests

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {MainMenuComponent} from "./main-menu/main-menu.component";
+import {
+  FunctionalCharacterMiniatureComponent
+} from "./functional-character-miniature/functional-character-miniature.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'main', component: MainMenuComponent},
+  { path: 'races', component: MainMenuComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
