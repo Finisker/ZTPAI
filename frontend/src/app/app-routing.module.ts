a696c87bda8c7430eaaf59df433fa4b3f18b9dea
maciejplachta <maciej.plachta@stazysta.comarch.pl>
2023-08-07 15:47:54 +0200

    Attributes-menu routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import {RacesMenuComponent} from "./races-menu/races-menu.component";
 import {ProfessionsMenuComponent} from "./professions-menu/professions-menu.component";
 import {SkillsMenuComponent} from "./skills-menu/skills-menu.component";
 import {AbilitiesMenuComponent} from "./abilities-menu/abilities-menu.component";
+import {AttributesMenuComponent} from "./attributes-menu/attributes-menu.component";
 
 const routes: Routes = [
   { path: 'characters', component: CharactersMenuComponent},
@@ -12,6 +13,7 @@ const routes: Routes = [
   { path: 'professions', component: ProfessionsMenuComponent},
   { path: 'skills', component: SkillsMenuComponent},
   { path: 'abilities', component: AbilitiesMenuComponent},
+  { path: 'attributes', component: AttributesMenuComponent},
 ];
 
 @NgModule({
