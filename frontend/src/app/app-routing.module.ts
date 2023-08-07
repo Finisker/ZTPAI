671e084d368a0d8e045fcf59876c36b5d3aa060e
maciejplachta <maciej.plachta@stazysta.comarch.pl>
2023-08-07 13:27:38 +0200

    Menus routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,17 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {MainMenuComponent} from "./characters-menu/characters-menu.component";
-import {
-  FunctionalCharacterMiniatureComponent
-} from "./functional-character-miniature/functional-character-miniature.component";
 import {RacesMenuComponent} from "./races-menu/races-menu.component";
+import {ProfessionsMenuComponent} from "./professions-menu/professions-menu.component";
+import {SkillsMenuComponent} from "./skills-menu/skills-menu.component";
+import {AbilitiesMenuComponent} from "./abilities-menu/abilities-menu.component";
 
 const routes: Routes = [
   { path: 'characters', component: MainMenuComponent},
   { path: 'races', component: RacesMenuComponent},
-  { path: 'professions', component: MainMenuComponent},
-  { path: 'skills', component: MainMenuComponent},
-  { path: 'abilities', component: MainMenuComponent},
+  { path: 'professions', component: ProfessionsMenuComponent},
+  { path: 'skills', component: SkillsMenuComponent},
+  { path: 'abilities', component: AbilitiesMenuComponent},
 ];
 
 @NgModule({
