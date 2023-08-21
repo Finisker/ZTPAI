0a5c2d5fb75bf047d07faad105ae6ca7590f14d4
maciejplachta <maciej.plachta@stazysta.comarch.pl>
2023-08-21 08:41:41 +0200

    /user routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {MainMenuComponent} from "./main-menu/main-menu.component";
+import {UserSettingsComponent} from "./user-settings/user-settings.component";
 
 const routes: Routes = [
   { path: 'main', component: MainMenuComponent},
+  { path: 'user', component: UserSettingsComponent},
 ];
 
 @NgModule({
