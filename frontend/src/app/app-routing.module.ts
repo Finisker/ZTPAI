1ef123efa3a12488e9b2afe9de01f69942bd2401
maciejplachta <maciej.plachta@stazysta.comarch.pl>
2023-08-21 14:51:52 +0200

    Login and Register routes

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {MainMenuComponent} from "./main-menu/main-menu.component";
+import {LoginComponent} from "./login/login.component";
+import {RegisterComponent} from "./register/register.component";
 
 const routes: Routes = [
   { path: 'main', component: MainMenuComponent},
+  { path: 'login', component: LoginComponent},
+  { path: 'register', component: RegisterComponent},
 ];
 
 @NgModule({
