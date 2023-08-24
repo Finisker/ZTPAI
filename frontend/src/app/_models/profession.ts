1951db569b0eac8aa317ffff77504319df5d85d5
maciejplachta <maciej.plachta@stazysta.comarch.pl>
2023-08-24 10:15:13 +0200

    Profession frontend model

---
+++ frontend/src/app/_models/profession.ts
@@ -0,0 +1,17 @@
+import {SocialClass} from "./socialClass";
+import {Ability} from "./ability";
+import {Attribute} from "./attribute";
+
+export class Profession{
+  name!: string;
+  path!: string;
+  description! : string;
+  socialStatus!: string;
+  equipment!: string;
+  level! : number;
+  socialClass!: SocialClass;
+
+  abilitySet!: Ability[];
+  talentSet!: Talent[];
+  attributeSet!: Attribute[];
+}
