c622e1cf0028d9d409f0a5a660dd279bc93b1671
maciejplachta <maciej.plachta@stazysta.comarch.pl>
2023-08-30 15:21:09 +0200

    Password authentication

--- frontend/src/app/authentication/register/register.component.ts
+++ frontend/src/app/authentication/register/register.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {User} from "../../_models/user";
+import {ActivatedRoute, Router} from "@angular/router";
+import {AuthService} from "../../_services/auth.service";
 
 @Component({
   selector: 'app-register',
@@ -6,5 +9,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
+  user: User;
 
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private userService: AuthService
+  )
+  {
+    this.user = new User();
+  }
+
+  registerUser() {
+    this.userService.save(this.user).subscribe(response =>{
+
+      console.log(response);
+      localStorage.setItem('token',response.token );
+      localStorage.setItem('refreshToken',response.refreshToken );
+      localStorage.setItem('id',response.uniqueId );
+      this.router.navigate(['/chat']);
+    });
+  }
 }
